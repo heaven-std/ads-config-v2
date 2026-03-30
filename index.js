353bb214e911f932587715527d9ae5320a8c55cb
heaven-std <quenv@tripsoft.io>
2026-03-30 14:15:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ import config from './config.json';
 
 export default {
   async fetch(request) {
-      return new Response(JSON.stringify({
-        allowed: !!config.packages[pkg]
-      }), {
-        headers: { "Content-Type": "application/json" }
-      });
-    }
-    return new Response(JSON.stringify(config), {
-      headers: { "Content-Type": "application/json" }
+     return new Response(JSON.stringify(config), {
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store"
+      }
     });
 };
